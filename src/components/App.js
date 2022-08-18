7a2d57a012f40b5bbcd6f316aaa9178e36b09f9c
Exion <70258985+HarashnabinRoy@users.noreply.github.com>
2022-08-18 18:49:28 +0530

    ADD Sponsors

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import Faq from './components/Faq';
+import TimeLine from './components/TimeLine';
+import Sponsors from './components/sponsors';
+function App() {
+  return (
+    <>
+      {/* <Faq /> */}
+      <TimeLine />
+      <Sponsors />
+    </>
+  );
+ 
+}
+
+export default App;
