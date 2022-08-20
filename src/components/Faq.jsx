fe998f49e22786bc30ac6c7142ceb2b440e36d52
siddhardha123 <siddhardha123123@gmail.com>
2022-08-20 14:11:03 +0530

    transition in faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -21,11 +21,11 @@ const Faq = () => {
           <div className="text-[3vmax] pb-8 text-[#7341AA] text-center"><h1>FAQ</h1></div>
           {data.map((item, i) => (
             <div className="item bg-[#0D0221] mb-5 px-10 py-5 rounded-md">
-              <div className="title flex justify-between items-center text-xl cursor-pointer text-white hover:text-[#B200CF]" onClick={() => toggle(i)}>
+              <div className="title flex justify-between items-center text-xl cursor-pointer duration-500 text-white hover:text-[#B200CF]" onClick={() => toggle(i)}>
                 <h1>{item.question}</h1>
-                <span className='text-2xl '>{selected === i ? '-' : '+'}</span>
+                <span className='text-2xl'>{selected === i ? '-' : '+'}</span>
               </div>
-              <div className={selected === i ? "content text-white text-xl pt-5" : "max-h-0 overflow-hidden"}>
+              <div className={selected === i ? "content text-white text-xl pt-5 transition ease-in duration-500" : "max-h-0 overflow-hidden transition ease-out duration-500"}>
                 {item.answer}
               </div>
             </div>
