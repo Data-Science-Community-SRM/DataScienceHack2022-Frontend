13d334272c9c87df1274ad99e002565e65580b45
sreyom31 <sreyom31@gmail.com>
2022-08-22 12:22:49 +0530

    devfolio button integration
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './assets/DShacklogo.svg';
 
-// React.useEffect(() => {
-//   const script = document.createElement('script');
-//   script.src = 'https://apply.devfolio.co/v2/sdk.js';
-//   script.async = true;
-//   script.defer = true;
-//   document.body.appendChild(script);
-//   return () => {
-//     document.body.removeChild(script);
-//   };
-// }, []);
-
 const Landing = () => {
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="h-[80vh] md:h-[90vh] w-[100vw]">
       <div className="logo h-[60%] flex justify-center mt-10">
         <img src={logo} alt="" className="w-[50%]" />
       </div>
-      <div className="text h-[40%] flex justify-center ">
-        <div className="text-left w-[70%] text-white text-[2vmax] md:text-[1.3vmax]">
+      <div className="text flex flex-col items-center justify-center ">
+        <div
+          className="apply-button"
+          data-hackathon-slug="dshack"
+          data-button-theme="light"
+          style={{ height: '44px', width: '312px' }}
+        >
+          Apply with Devfolio
+        </div>
+        <div className="text-left w-[70%] text-white text-[2vmax] md:text-[1.3vmax] mt-16">
           {' '}
           &apos; We cannot solve our problems with the same THINKING we used
           when we created them&apos;
@@ -30,14 +38,6 @@ const Landing = () => {
           will provide an opportunity for hackers to shape their ideas into
           reality
         </div>
-        {/* <div
-          className="apply-button"
-          data-hackathon-slug="dshack"
-          data-button-theme="light"
-          style="height: 44px; width: 312px"
-        >
-          Apply with Devfolio
-        </div> */}
       </div>
     </div>
   );
