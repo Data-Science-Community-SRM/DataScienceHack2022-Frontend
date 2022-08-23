39176e1a5660564cd69a928f69b4e95c3f1dc505
sreyom31 <sreyom31@gmail.com>
2022-08-23 19:59:47 +0530

    timeline work around
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import timeline from './assets/timeline.png';
 const TimeLine = () => {
   return (
     <>
@@ -6,14 +7,14 @@ const TimeLine = () => {
         className="flex h-auto w-[100vw] justify-center items-center my-8"
         id="timeline"
       >
-        <div className="timeline w-[60%]">
+        <div className="timeline w-[60%] flex flex-col items-center">
           <div className="flex justify-center relative mb-16">
             <p className="text-[#B200CF] text-[3vmax] font-bold mt-20 transform -translate-y-1">
               TIMELINE
             </p>
             <div className="w-[180px] h-16 bottom-0 absolute"></div>
           </div>
-          {data.map((item, i) => (
+          {/* {data.map((item, i) => (
             <div className="md:flex text-[#B200CF] text-xl" key={i}>
               <div className="w-[100%] mb-10 md:w-[20%]">
                 Day {item.day}
@@ -39,60 +40,61 @@ const TimeLine = () => {
                 ))}
               </div>
             </div>
-          ))}
+          ))} */}
+          <img src={timeline} alt="" />
         </div>
       </div>
     </>
   );
 };
-const data = [
-  {
-    date: 'Thu, Sept 1st',
-    day: '1',
-    events: [
-      {
-        time: null,
-        event_title: 'Round 1 (Competitive coding)',
-      },
-    ],
-  },
-  {
-    date: 'Sat, Sept 3rd',
-    day: '2',
-    events: [
-      {
-        time: null,
-        event_title: 'Round 2 (Project Build)',
-      },
-      {
-        time: '0900 hrs',
-        event_title: ' The hackathon begins',
-      },
-    ],
-  },
-  {
-    date: 'Sun, Sept 4th',
-    day: '3',
-    events: [
-      {
-        time: '1200 hrs',
-        event_title: 'The hackathon ends.',
-      },
-      {
-        time: null,
-        event_title: 'Round 3 (Presentation)',
-      },
-      {
-        time: '1300 hrs',
-        event_title:
-          ' The results for the 2nd round will go up on the website & the participants will also receive a mail',
-      },
-      {
-        time: '1400 hrs',
-        event_title:
-          ' The Finals will occur on "4th Sept", where the participants will have to illustrate their project via a presentation',
-      },
-    ],
-  },
-];
+// const data = [
+//   {
+//     date: 'Thu, Sept 1st',
+//     day: '1',
+//     events: [
+//       {
+//         time: null,
+//         event_title: 'Round 1 (Competitive coding)',
+//       },
+//     ],
+//   },
+//   {
+//     date: 'Sat, Sept 3rd',
+//     day: '2',
+//     events: [
+//       {
+//         time: null,
+//         event_title: 'Round 2 (Project Build)',
+//       },
+//       {
+//         time: '0900 hrs',
+//         event_title: ' The hackathon begins',
+//       },
+//     ],
+//   },
+//   {
+//     date: 'Sun, Sept 4th',
+//     day: '3',
+//     events: [
+//       {
+//         time: '1200 hrs',
+//         event_title: 'The hackathon ends.',
+//       },
+//       {
+//         time: null,
+//         event_title: 'Round 3 (Presentation)',
+//       },
+//       {
+//         time: '1300 hrs',
+//         event_title:
+//           ' The results for the 2nd round will go up on the website & the participants will also receive a mail',
+//       },
+//       {
+//         time: '1400 hrs',
+//         event_title:
+//           ' The Finals will occur on "4th Sept", where the participants will have to illustrate their project via a presentation',
+//       },
+//     ],
+//   },
+// ];
 export default TimeLine;
