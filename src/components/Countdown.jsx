9505160f56023464120d8bf74847c6627656ab35
Exion <70258985+HarashnabinRoy@users.noreply.github.com>
2022-08-29 10:36:20 +0530

    Update Timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -14,7 +14,7 @@ const Countdown = () => {
       const total = Date.parse(endtime) - Date.parse(new Date());
       const seconds = Math.floor((total / 1000) % 60);
       const minutes = Math.floor((total / 1000 / 60) % 60);
-      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24)-6;
       const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
       return {
